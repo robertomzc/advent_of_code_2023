abc83c2392dfe61dae9f3db3cc989c2b2de8eec2
r.martinezdelacruz <roberto.martinezdelacruz@ageagle.com>
2024-01-05 18:50:52 +0100

    day 4 part 2

--- days/day4/day4Main.cpp
+++ days/day4/day4Main.cpp
@@ -3,10 +3,14 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <array>
+#include <numeric>
 #include "../../common/pstring.h"
 #include "../../common/text_file.h"
 
 using namespace std;
+
+int PART = 2;
 
 class CardGame {
 private:
@@ -27,9 +31,13 @@ public:
     int game_id;
     vector <int> win_numbers;
     vector <int> game_numbers;
+    int successes;
     int points;
 
-    CardGame(const string& content) : content(content), p_content(content){
+    explicit CardGame(const string& content) : content(content), p_content(content){
+        findGameID();
+        findWinNumbers();
+        findGameNumbers();
     }
 
     void findGameID() {
@@ -50,22 +58,25 @@ public:
         game_numbers = transformSpacedNumbersToIntVector(game_numbers_str);
     }
 
-    void countPoints() {
-        findWinNumbers();
-        findGameNumbers();
-
-        int number_of_success = 0;
+    void countNumberOfSuccesses() {
+        // Count how many of your game numbers are in the win numbers
+        successes = 0;
         for (int win_number : win_numbers) {
             if (find(game_numbers.begin(), game_numbers.end(), win_number) != game_numbers.end()) {
-                number_of_success++;
+                successes++;
             }
+        }
+    }
 
-            if (number_of_success == 0) {
-                points = 0;
-            }
-            else {
-                points = (int) pow(2, number_of_success - 1);
-            }
+    void countPoints() {
+
+        countNumberOfSuccesses();
+
+        if (successes == 0) {
+            points = 0;
+        }
+        else {
+            points = (int) pow(2, successes - 1);
         }
     }
 };
@@ -74,18 +85,43 @@ int main() {
     TextFile f("/home/rmartinezdelacruz/Code/personal/advent_of_code_2023/days/day4/input4");
     int total_points = 0;
 
-    while (true) {
-        string line = f.next();
 
-        if (line.empty()) {
-            break;
+    if (PART == 1) {
+        while (true) {
+            string line = f.next();
+
+            if (line.empty()) {
+                break;
+            }
+
+            CardGame card_game(line);
+            card_game.countPoints();
+            total_points += card_game.points;
         }
 
-        CardGame card_game(line);
-        card_game.countPoints();
-        total_points += card_game.points;
-
+    cout << total_points;
     }
 
-    cout << total_points;
+    else if (PART == 2) {
+        vector<CardGame> all_cards;
+        while (true) {
+            string line = f.next();
+
+            if (line.empty()) {
+                break;
+            }
+            CardGame card_game(line);
+            card_game.countNumberOfSuccesses();
+            all_cards.push_back(card_game);
+        }
+
+        vector<int> number_of_each_card(all_cards.size(), 1);
+        for (int i=0; i<all_cards.size(); i++) {
+            for (int j=1; j<=all_cards[i].successes; j++) {
+                // Add 1*number_of_i_cards to the next i+j card
+                number_of_each_card[i+j] += 1*number_of_each_card[i];
+            }
+        }
+        cout << accumulate(number_of_each_card.begin(), number_of_each_card.end(), 0);
+    }
 }
